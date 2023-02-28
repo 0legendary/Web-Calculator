39214e065257d4aca6358b9ada654122ec968fb9
Alen.m <alenmullassery123@gmail.com>
2023-02-28 12:39:57 +0530

    Developed all action for each button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var calcState = {}
 function buttonclick(val)
 {
     document.getElementById("screen").value+=val
@@ -5,21 +6,53 @@ function buttonclick(val)
 function clearDisplay(){
     document.getElementById("screen").value=""
 }
-function clearDisplayByOne(val){
-    document.getElementById("screen").value-=val
+function clearDisplayByOne(){
+    var current_value = document.getElementById("screen").value
+    var new_value = current_value.slice(0, -1); // remove the last character
+  document.getElementById("screen").value = new_value;
 }
 function equalClick(){
-    var second_num=document.getElementById("screen").value
+    var second_num= parseFloat(document.getElementById("screen").value)
     document.getElementById("screen").value=""
-    if (math=="Division"){
-        document.getElementById("screen").value+=f_num/second_num
+    if (calcState.math=="Division"){
+        result= calcState.f_num / second_num
+        document.getElementById("screen").value=result
+    }else if(calcState.math=="Addition"){
+        result= calcState.f_num + second_num
+        document.getElementById("screen").value=result
+    }else if(calcState.math=="Subtraction"){
+        result= calcState.f_num - second_num
+        document.getElementById("screen").value=result
+    }else if(calcState.math=="Multiplication"){
+        result= calcState.f_num * second_num
+        document.getElementById("screen").value=result
+    }else{
+        document.getElementById("screen").value="Error"
     }
+
+
 }
 function buttonDiv(){
-    var first_num=document.getElementById("screen")
-    var myGlobalVar= "math"
-    var myGlobalVar= "f_num"
-    math="Division"
-    f_num= first_num
+    var first_num=parseFloat(document.getElementById("screen").value)
+    calcState.math="Division"
+    calcState.f_num = first_num
+    document.getElementById("screen").value=""
+}
+function buttonAdd(){
+    var first_num=parseFloat(document.getElementById("screen").value)
+    calcState.math="Addition"
+    calcState.f_num = first_num
+    document.getElementById("screen").value=""
+}
+function buttonSub(){
+    var first_num=parseFloat(document.getElementById("screen").value)
+    calcState.math="Subtraction"
+    calcState.f_num = first_num
+    document.getElementById("screen").value=""
+}
+function buttonMult(){
+    var first_num=parseFloat(document.getElementById("screen").value)
+    calcState.math="Multiplication"
+    calcState.f_num = first_num
     document.getElementById("screen").value=""
 }
